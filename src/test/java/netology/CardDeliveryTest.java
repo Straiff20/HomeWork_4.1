c59dce906f269c97518313160e6d17196f57c96f
Ilya Pliev <straiff20@gmail.com>
2020-02-07 09:28:13 +0300

    Add replan test

--- src/test/java/netology/CardDeliveryTest.java
+++ src/test/java/netology/CardDeliveryTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static netology.Data.Registration;
@@ -13,7 +14,8 @@ import static netology.Data.methodTimestamp;
 public class CardDeliveryTest {
 
 
-    // Доставка в Сочи, Тольяти не доступка - бага
+    // Доставка в Сочи, Тольяти не доступно - бага
+    // Зинаида Артёмовна Кулакова (буква Ё)
     @Test
     void CardHappyTest() {
         open("http://localhost:9999/");
@@ -33,5 +35,33 @@ public class CardDeliveryTest {
         Constants.succesNotify.waitUntil(Condition.visible, 15000);
         Constants.succesNotify.shouldHave(text("Успешно!"));
         Constants.succesNotify.shouldHave(text("Встреча успешно запланирована на " + date));
+    }
+
+    @Test
+    void CardReplanDate() throws InterruptedException {
+        open("http://localhost:9999/");
+        RegCardInfo myForm = Registration.generateByCard("ru");
+        Constants.mainTitle.isDisplayed();
+        Constants.cityField.setValue(myForm.getCity());
+        Constants.dateField.click();
+        Constants.calendar.isDisplayed();
+        long extendedTimestamp = methodTimestamp(Constants.future_date);
+        String extended = String.valueOf(extendedTimestamp);
+        $("tr.calendar__row  td.calendar__day[data-day=\"" + extended + "\"]").click();
+        String date = Constants.dateField.getText();
+        Constants.nameField.setValue(myForm.getFullName());
+        Constants.phoneNumberField.setValue(myForm.getPhoneNumber());
+        Constants.iAgreeCheckBox.click();
+        Constants.confirmButton.click();
+        Constants.succesNotify.waitUntil(Condition.visible, 15000);
+        Constants.succesNotify.shouldHave(text("Успешно!"));
+        Constants.succesNotify.shouldHave(text("Встреча успешно запланирована на " + date));
+
+        Constants.confirmButton.click();
+        Thread.sleep(2000);
+        Constants.replanNotify.waitUntil(visible, 15000);
+        Constants.replanNotify.shouldHave(text("Необходимо подтверждение"));
+        Constants.replanNotify.shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        Constants.replanButton.isDisplayed();
     }
 }
--- src/test/java/netology/Constants.java
+++ src/test/java/netology/Constants.java
@@ -6,15 +6,17 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$;
 
 public class Constants {
-    public static final int future_date = 7;  //  через сколько дней
+    static final int future_date = 7;  //  через сколько дней запланировать доставку
 
-    static SelenideElement mainTitle = $(".heading");  //  Заголовок
-    static SelenideElement cityField = $("[placeholder=Город]");  //  поле ввода города
-    static SelenideElement dateField = $("[placeholder='Дата встречи']");  //  поле ввода даты
-    static SelenideElement calendar = $(".calendar[role=grid]");  //  всплывающий календарь
-    static SelenideElement nameField = $("[name=name]");  //  поле ввода имени
-    static SelenideElement phoneNumberField = $("[name=phone]");  //  поле ввода номера телефона
-    static SelenideElement iAgreeCheckBox = $("span.checkbox__box");  //  чекбокс для галочки "я согласен"
-    static SelenideElement confirmButton = $("span.button__text");  //  кнопка "Запланировать"
-    static SelenideElement succesNotify = $("[data-test-id='success-notification']");  //  popup окно с "Успешно!"
+    static final SelenideElement mainTitle = $(".heading");  //  Заголовок
+    static final SelenideElement cityField = $("[placeholder=Город]");  //  поле ввода города
+    static final SelenideElement dateField = $("[placeholder='Дата встречи']");  //  поле ввода даты
+    static final SelenideElement calendar = $(".calendar[role=grid]");  //  всплывающий календарь
+    static final SelenideElement nameField = $("[name=name]");  //  поле ввода имени
+    static final SelenideElement phoneNumberField = $("[name=phone]");  //  поле ввода номера телефона
+    static final SelenideElement iAgreeCheckBox = $("span.checkbox__box");  //  чекбокс для галочки "я согласен"
+    static final SelenideElement confirmButton = $("span.button__text");  //  кнопка "Запланировать"
+    static final SelenideElement succesNotify = $("[data-test-id='success-notification']");  //  popup окно с "Успешно!"
+    static final SelenideElement replanNotify = $("[data-test-id='replan-notification']");
+    static final SelenideElement replanButton = $("div.notification__content > button > span > span.button__text");
 }
